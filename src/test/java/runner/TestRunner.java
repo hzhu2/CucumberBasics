47f586dab258ea39467474bcb555c5e2f7b8e301
han.z <hzhu@palo-it.com>
2018-07-30 14:53:02 +0800

    Add basic reporting

--- src/test/java/runner/TestRunner.java
+++ src/test/java/runner/TestRunner.java
@@ -11,7 +11,7 @@ import org.junit.runner.RunWith;
 public class TestRunner {
 }*/
 
-@CucumberOptions(features = {"src/test/java/features"}, glue = "Steps")
+@CucumberOptions(features = {"src/test/java/features"}, format = {"json:target/cucumber.json", "html:target/site/cucumber-pretty"}, glue = "Steps")
 public class TestRunner extends AbstractTestNGCucumberTests{
 
 }
